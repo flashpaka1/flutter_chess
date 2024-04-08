e49d379a43ef655195bee455a5a0c8a27316adf5
Daniel Billingsley <d.billingsley@eagles.oc.edu>
2024-04-08 16:27:46 -0500

    update(pieces): started adding pieces

--- chess_app/lib/main.dart
+++ chess_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -27,7 +29,7 @@ class MyChessGame extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'The Great Game', 
+          'Don\'t Lose', 
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Color.fromARGB(255, 36, 36, 36),
@@ -66,27 +68,101 @@ class ChessBoard extends StatelessWidget {
   }
 
   Widget _buildSquare(int row, int col) {
-    Color lightColor = Color.fromARGB(255, 255, 242, 211);
-    Color darkColor = Color.fromARGB(255, 26, 159, 66);
-    Color color = (row + col) % 2 == 0 ? lightColor : darkColor!;
-    return Expanded(
-      child: Container(
-        color: color,
-        child: InkWell(
-          onTap: () {
-            // Add functionality for tapping squares here
-            print('Clicked square: ${String.fromCharCode(col + 97)}${8 - row}');
+  Color lightColor = const Color.fromARGB(255, 255, 242, 211);
+  Color darkColor = const Color.fromARGB(255, 26, 159, 66);
+  Color color = (row + col) % 2 == 0 ? lightColor : darkColor!;
+  return Expanded(
+    child: Container(
+      color: color,
+      child: InkWell(
+        onTap: () {
+          // Add functionality for tapping squares here
+          print('Clicked square: ${String.fromCharCode(col + 97)}${8 - row}');
+        },
+        child: DragTarget<ChessPiece>(
+          builder: (context, candidateData, rejectedData) {
+            // Return an empty Container if no piece is present, otherwise return the piece
+            return _getPieceAtPositon(row, col) ?? Container();
           },
-          child: Center(
-            child: Text(
-              '${String.fromCharCode(col + 97)}${8 - row}',
-              style: TextStyle(
-                color: color == lightColor ? Colors.black : Colors.white,
-              ),
-            ),
-          ),
+          onWillAccept: (data) {
+            return true;
+          },
+          onAccept: (data) {
+            setState(() {
+              data!.position = Position(row, col);
+            });
+          },
         ),
+      ),
+    ),
+  );
+}
+
+
+  Widget _getPieceAtPositon(int row, int col) {
+    ChessPiece? piece = pieces.firstWhere((p) => p.position.row == row && p.position.col == col);
+    if (piece != null){
+      return Draggable<ChessPiece>(
+        data: piece,
+        child: _buildPiece(piece),
+        feedback: _buildPiece(piece),
+        childWhenDragging: Container(),
+      );
+    }
+    else {
+      return Container();
+    }
+  }
+  Widget _buildPiece(ChessPiece piece) {
+    return Text(
+      piece.type.toString().substring(10), // Display piece type
+      style: TextStyle(
+        fontSize: 20,
+        color: piece.color == PieceColor.white ? Colors.black : Colors.white,
       ),
     );
   }
+ 
+  List<ChessPiece> pieces = [
+    // white's pieces
+    // pawns
+    ChessPiece(type: PieceType.pawn, color: PieceColor.white, position: Position(1,0)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.white, position: Position(1,1)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,2)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,3)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,4)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,5)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,6)),
+    ChessPiece(type: PieceType.pawn, color: PieceColor.black, position: Position(1,7)),
+  ];
+  
+  void setState(Null Function() param0) {}
+}
+ class ChessPiece {
+    final PieceType type;
+    final PieceColor color;
+    final Position position;
+
+    ChessPiece({required this.type, required this.color, required this.position});
+  }
+  
+  class Position {
+    final int row;
+    final int col;
+    
+    Position(this.row, this.col);
+  }
+
+enum PieceType {
+  pawn,
+  bishop,
+  knight,
+  rook,
+  queen,
+  king
+}
+
+enum PieceColor {
+  white,
+  black
 }
