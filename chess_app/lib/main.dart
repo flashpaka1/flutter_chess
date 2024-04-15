260223f378b1787a582c67f56c947632fc8a0e4c
theshaggy5000 <nickdrebot@gmail.com>
2024-04-15 18:56:05 -0500

    added app to firebase

--- chess_app/lib/main.dart
+++ chess_app/lib/main.dart
@@ -4,10 +4,16 @@ import 'package:chess_app/position.dart';
 import 'package:flutter/material.dart';
 import 'package:chess_app/chess_pieces.dart';
 import 'package:chess_app/pages/login_page.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Game engine = Game();
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
